e44460cb2efc93311f0d6b55ed4c91636d7daaab
Maxwell Lanzkron-Tamarazo <maxwell.lt@live.com>
2017-01-13 12:46:31 -0500

    Started work on decay mechanic
    blockstates json is erroring for unknown reasons
    textures added to project

--- src/main/java/maxwell_lt/mobblocker/blocks/BlockChunkProtector.java
+++ src/main/java/maxwell_lt/mobblocker/blocks/BlockChunkProtector.java
@@ -4,6 +4,9 @@ import maxwell_lt.mobblocker.MobBlocker;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -15,6 +18,8 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockChunkProtector extends Block implements ITileEntityProvider {
+	
+	public static PropertyInteger DECAYLEVEL = PropertyInteger.create("decay", 0, 2);
     
 	public BlockChunkProtector() {
         super(Material.ROCK);
@@ -35,6 +40,22 @@ public class BlockChunkProtector extends Block implements ITileEntityProvider {
 	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return new TileEntityChunkProtector();
+	}
+	
+	@Override
+	public BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, DECAYLEVEL);
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(DECAYLEVEL, meta);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(DECAYLEVEL);
+		
 	}
 	
 	
