113f5e5bd96351930af5c63ec48934d6922921c1
Maxwell Lanzkron-Tamarazo <maxwell.lt@live.com>
2017-01-14 23:40:44 -0500

    Implemented gifting the player a Chunk Protector on login.
    Closes #2

--- src/main/java/maxwell_lt/mobblocker/LoginEventHandler.java
+++ src/main/java/maxwell_lt/mobblocker/LoginEventHandler.java
@@ -1,17 +1,37 @@
 package maxwell_lt.mobblocker;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber
 public class LoginEventHandler {
 	
 	@SubscribeEvent
-	public void playerLogin(EntityJoinWorldEvent login) {
-		if (login.getEntity() instanceof EntityPlayer && !login.getWorld().isRemote) {
-			EntityPlayer player = (EntityPlayer) login.getEntity();
-			//WIP
+	public void onPlayerJoinWorld(EntityJoinWorldEvent event) {
+		if (!event.getEntity().world.isRemote && event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			NBTTagCompound persistentTag;
+			if (player.getEntityData().hasKey(EntityPlayer.PERSISTED_NBT_TAG)) {
+				persistentTag = player.getEntityData().getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+			} else {
+				persistentTag = new NBTTagCompound();
+				player.getEntityData().setTag(EntityPlayer.PERSISTED_NBT_TAG, persistentTag);
+			}
+			NBTTagCompound tag;
+			if (persistentTag.hasKey("mobblocker")) {
+				tag = persistentTag.getCompoundTag("mobblocker");
+			} else {
+				tag = new NBTTagCompound();
+				persistentTag.setTag("mobblocker", tag);
+			}
+			if (!tag.getBoolean("receivedprotector")) {
+				player.inventory.addItemStackToInventory(new ItemStack(ModBlocks.chunkProtector));
+				tag.setBoolean("receivedprotector", true);
+			}
 		}
 	}
-
 }
--- src/main/java/maxwell_lt/mobblocker/MobBlocker.java
+++ src/main/java/maxwell_lt/mobblocker/MobBlocker.java
@@ -1,6 +1,8 @@
 package maxwell_lt.mobblocker;
 
 import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -31,6 +33,7 @@ public class MobBlocker {
     @Mod.EventHandler
     public void init(FMLInitializationEvent e) {
         proxy.init(e);
+        MinecraftForge.EVENT_BUS.register(new LoginEventHandler());
     }
 
     @Mod.EventHandler
